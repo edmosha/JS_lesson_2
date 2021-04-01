6e73143442d97c73fdcf431593fd6a850118a0d6
Edmosha <dasha1234495@gmail.com>
2021-04-02 00:51:07 +0300

    Комент теперь имеет автора и дату отправления Появилась сортировка по дате

--- main.js
+++ main.js
@@ -1,64 +1,120 @@
-// Подсказка №1: Содержимое тега textarea хранится в его свойстве value
-// Подсказка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
-
-
-
-// план действий
-// 1. отловить нажатие кнопки добавить
-// - происходит в событии п5
-
-// 2. сохранить значение из поля текстерии
-//const commentContent = document.getElementById('comment').value
-
-// 3. сохранить в локалсторадж
-// localStorage.setItem('first_comment', commentContent.toString())
-
-// 4. очистить поле
 let comments = []
-let num
 
-// 5. добавить блок комента
-document.getElementById('addComment').addEventListener("click", () => {
-    const newComment = document.getElementById('comment').value
-    comments.push(newComment)
+class CommentsClass {
+    constructor(author, date, content) {
+        this.author = author
+        this.date = date
+        this.content = content
+    }
+}
+// ---------------------
+// Не понимаю, как удалять и редактировать коменты..
+// ---------------------
+console.log(comments)
+
+document.getElementById('addComment').addEventListener('click', ClickButton => {
+    AddCommentToLocalStorage()
     console.log(comments)
-
-    localStorage.setItem('comments', JSON.stringify(comments))
-
-    comments = JSON.parse(localStorage.getItem('comments'))
-    console.log(comments)
-    num = comments.length // номер ласт элемента массива
-    console.log(num)
-    console.log(comments[num-1])
-
-    document.querySelector('.wrapper').insertAdjacentHTML("beforeend",
-        `<p class="commentBlock"> 
-                <span class="commentMark">Пуськин поклонник &#128571; </span><br>
-                <span>` + comments[num-1] +`</span></p>`)
-    const clearElem = document.getElementById('comment')
-    clearElem.value = ''
-
+    AddCommentToPage()
 })
 
+//document.getElementById('delete').addEventListener('click', () => DeleteComment())
+
 if(localStorage.getItem('comments')) {
-    Show()
+    comments = JSON.parse((localStorage.getItem('comments')))
+    ShowComments()
 }
 
+function AddCommentToLocalStorage() {
+    let d = new Date();
+    let newDateText = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear() + ' в ' + d.getHours() + ':' + d.getMinutes()
+    let newAuthorText = document.getElementById('author').value
+    let newCommentText = document.getElementById('comment').value
 
+    let newComment = new CommentsClass(newAuthorText, newDateText, newCommentText)
+    comments.push(newComment)
+    localStorage.setItem('comments', JSON.stringify(comments))
+}
 
-function Show() {
+function AddCommentToPage() {
+    comments = JSON.parse(localStorage.getItem('comments'))
+
+    document.querySelector('.wrapper').insertAdjacentHTML("beforeend",
+        `<div class="commentBlock">
+            <div class="commBoxTop">
+                <div>
+                    <span class="commentMark">` + comments[comments.length-1].author + `</span>
+                    <span class="date">` + comments[comments.length-1].date + `</span>
+                </div>
+                <button id="edit" onclick="DeleteComment()">Редактировать</button>
+                <button class="del">Удалить</button>
+            </div>
+            <span>` + comments[comments.length-1].content + `</span>
+        </div>`)
+
+    const clearElem = document.getElementById('comment')
+    const clearElem1 = document.getElementById('author')
+    clearElem.value = ''
+    clearElem1.value = ''
+}
+
+function ShowComments() {
     comments = JSON.parse((localStorage.getItem('comments')))
+    // ClearComment()
     for(let i=0; i<comments.length; i++) {
-        console.log(comments[i])
-
         document.querySelector('.wrapper').insertAdjacentHTML("beforeend",
-            `<p class="commentBlock"> 
-                <span class="commentMark">Пуськин поклонник &#128571; </span><br>
-                <span>` + comments[i] +`</span></p>`)
+            `<div class="commentBlock">
+            <div class="commBoxTop">
+                <div>
+                    <span class="commentMark">` + comments[i].author + `</span>
+                    <span class="date">` + comments[i].date + `</span>
+                </div>
+                <button id="edit">Редактировать</button>
+                <button id="delete">Удалить</button>
+            </div>
+            <span>` + comments[i].content + `</span>
+        </div>`)
 
     }
 }
 
+//function DeleteComment() {
+//
+//}
+function ClearComment() {
+    for(let i=0; i<comments.length; i++) {
+        let elem = document.querySelector('.commentBlock')
+        elem.remove()
+    }
+}
+
+function SortByDescendingDate() {
+    comments = JSON.parse((localStorage.getItem('comments')))
+
+    // comments.forEach(document.querySelector('.commentBlock'),function (elem) {
+    //     elem.remove()
+    // })
+
+    ClearComment()
+
+    let i = comments.length
+    while (i !== 0) {
+        document.querySelector('.wrapper').insertAdjacentHTML("beforeend",
+            `<div class="commentBlock">
+            <div class="commBoxTop">
+                <div>
+                    <span class="commentMark">` + comments[i-1].author + `</span>
+                    <span class="date">` + comments[i-1].date + `</span>
+                </div>
+                <button id="edit">Редактировать</button>
+                <button id="delete">Удалить</button>
+            </div>
+            <span>` + comments[i-1].content + `</span>
+        </div>`)
+        i -= 1
+
+    }
+}
 
 
 
